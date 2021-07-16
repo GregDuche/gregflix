f6f0140c315938929e82ed7eb7784f1ea95607d4
Duché, Grégoire <Gregoire.Duche@news.com.au>
2021-07-16 10:22:08 +1000

    Minor changes

--- src/Api.js
+++ src/Api.js
@@ -19,7 +19,7 @@ const movies = [
   },
   {
     title: 'Parkour',
-    poster: '/img/movieData/01/movie01.png',
+    poster: '/img/movieData/02/movie02.png',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vitae imperdiet libero, semper rutrum mi. Suspendisse elementum odio ornare suscipit semper. ',
     promoted: false,
     src: 'https://bitdash-a.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8',
